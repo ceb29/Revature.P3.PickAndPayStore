b428c944f21e8e19c7771b3489f275d636c83bc4
Curtis Becker <ceb2941@gmail.com>
2022-06-28 18:03:10 -0400

    added helper functions for collections

--- Revature.P3.PickAndPayStore/HomePage/HomeCollectionHelper.swift
+++ Revature.P3.PickAndPayStore/HomePage/HomeCollectionHelper.swift
@@ -6,15 +6,31 @@
 //
 
 import Foundation
+import UIKit
 
-func setupHomeProductCollectionCell(){
-    
+var productData = ["test1", "test2", "test3", "test4", "test5"]
+var promoData = ["img1"]
+var recommendedData = ["test", "test", "test", "test"] //should recommended even be collection view
+
+func setupHomeProductCollectionCell(_ homeProductCell : HomeProductCollectionViewCell, _ indexPath : IndexPath) -> UICollectionViewCell{
+    homeProductCell.productLabel.text = productData[indexPath.item]
+    //homeProductCell.productImage.image = UIImage(named: productData[indexPath.item])
+    return setupRoundedCellCorners(homeProductCell)
 }
 
-func setupHomePromoCollectionCell(){
-    
+func setupHomePromoCollectionCell(_ homePromoCell : HomePromoCollectionViewCell, _ indexPath : IndexPath) -> UICollectionViewCell{
+    homePromoCell.promoImage.image = UIImage(named: promoData[indexPath.item])
+    return setupRoundedCellCorners(homePromoCell)
 }
 
-func setupHomeRecommendedCollectionCell(){
-    
+func setupHomeRecommendedCollectionCell(_ homeRecommendedCell : HomeRecommendedCollectionViewCell, _ indexPath : IndexPath) -> UICollectionViewCell{
+    homeRecommendedCell.recommendedLabel.text = recommendedData[indexPath.item]
+    //homeRecommendedCell.recommendedImage.image = UIImage(named: recommendedData[indexPath.item])
+    return setupRoundedCellCorners(homeRecommendedCell)
+}
+
+func setupRoundedCellCorners(_ collectionViewCell : UICollectionViewCell) -> UICollectionViewCell{
+    collectionViewCell.layer.cornerRadius = 10
+    collectionViewCell.layer.masksToBounds = true
+    return collectionViewCell
 }
--- Revature.P3.PickAndPayStore/HomePage/HomeViewController.swift
+++ Revature.P3.PickAndPayStore/HomePage/HomeViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate{
-    var productData = ["test1", "test2", "test3", "test4", "test5"]
-    var promoData = ["img1"]
-    var recommendedData = ["test", "test", "test", "test", "test"]
+    //var productData = ["test1", "test2", "test3", "test4", "test5"]
+    //var promoData = ["img1"]
+    //var recommendedData = ["test", "test", "test", "test"]
+    @IBOutlet weak var bottomPromoImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bottomPromoImage.image = UIImage(named: "img1")
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         switch collectionView.restorationIdentifier{
@@ -24,20 +30,20 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let currentCell : UICollectionViewCell
         switch collectionView.restorationIdentifier{
         case "homeProducts":
-            currentCell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeProductCell", for: indexPath)
+            let currentCell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeProductCell", for: indexPath) as! HomeProductCollectionViewCell
+            return setupHomeProductCollectionCell(currentCell, indexPath)
         case "homePromos":
-            currentCell = collectionView.dequeueReusableCell(withReuseIdentifier: "homePromoCell", for: indexPath)
+            let currentCell = collectionView.dequeueReusableCell(withReuseIdentifier: "homePromoCell", for: indexPath) as! HomePromoCollectionViewCell
+            return setupHomePromoCollectionCell(currentCell, indexPath)
         default:
-            currentCell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeRecommendedCell", for: indexPath)
+            let currentCell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeRecommendedCell", for: indexPath) as! HomeRecommendedCollectionViewCell
+            return setupHomeRecommendedCollectionCell(currentCell, indexPath)
         }
-        return currentCell
+        
 
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    
 }
