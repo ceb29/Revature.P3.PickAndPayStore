3783d3c4f2ef5ab2230b695217919b75c6a1eb5f
Curtis Becker <ceb2941@gmail.com>
2022-06-30 16:07:45 -0400

    added models and view models

---
+++ Revature.P3.PickAndPayStore/ViewModels/ProductCategoryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCategoryViewModel.swift
+//  Revature.P3.PickAndPayStore
+//
+//  Created by admin on 6/30/22.
+//
+
+import Foundation
+
+struct ProductCategoryViewModel{
+    var name : String
+    var image : String
+}
---
+++ Revature.P3.PickAndPayStore/ViewModels/PromoViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PromoViewModel.swift
+//  Revature.P3.PickAndPayStore
+//
+//  Created by admin on 6/30/22.
+//
+
+import Foundation
+
+struct PromoViewModel{
+    var name : String
+    var image : String
+}
