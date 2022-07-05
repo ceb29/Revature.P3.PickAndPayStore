0fcd9a9fd074fe450514be386ab39dbba8db8b45
MaxStump13 <stumpmaximilian@gmail.com>
2022-07-05 09:30:24 -0400

    Added the background image to the sign in and login

---
+++ Revature.P3.PickAndPayStore/SignIn/SignInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignInViewController.swift
+//  Revature.P3.PickAndPayStore
+//
+//  Created by Maximilian Stump on 7/5/22.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
