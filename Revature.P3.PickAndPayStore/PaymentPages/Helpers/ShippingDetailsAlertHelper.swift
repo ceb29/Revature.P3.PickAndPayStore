cceaf11041f3c1734ad41701a28c78e3a5c40579
Curtis Becker <ceb2941@gmail.com>
2022-07-07 17:06:42 -0400

    added regex

--- Revature.P3.PickAndPayStore/PaymentPages/Helpers/ShippingDetailsAlertHelper.swift
+++ Revature.P3.PickAndPayStore/PaymentPages/Helpers/ShippingDetailsAlertHelper.swift
@@ -13,4 +13,29 @@ struct ShippingDetailsAlertHelper{
         let zipCodePred = NSPredicate(format: "SELF MATCHES %@", zipCodeRegEx)
         return zipCodePred.evaluate(with: zipCode)
     }
+    
+    func isValidAddress(address: String) -> Bool{
+        let addressRegEx = "^[0-9A-Za-z .'#@%&/-]*$"
+        let addressPred = NSPredicate(format: "SELF MATCHES %@", addressRegEx)
+        return addressPred.evaluate(with: address)
+    }
+    
+    func isValidCity(city: String) -> Bool{
+        let cityRegEx = "^[A-Za-z .'-]*$"
+        let cityPred = NSPredicate(format: "SELF MATCHES %@", cityRegEx)
+        return cityPred.evaluate(with: city)
+    }
+    /*
+    func isValidState(state: String) -> Bool{
+        let stateRegEx = "^[A-Za-z ]$"
+        let statePred = NSPredicate(format: "SELF MATCHES %@", stateRegEx)
+        return statePred.evaluate(with: state)
+    }
+    
+    func isValidCountry(country: String) -> Bool{
+        let countryRegEx = "^[A-Za-z ]$"
+        let countryPred = NSPredicate(format: "SELF MATCHES %@", countryRegEx)
+        return countryPred.evaluate(with: country)
+    }
+    */
 }
