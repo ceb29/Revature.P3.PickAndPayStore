4170b3011a6fa09e251c984e53e4bfb6a7aca85b
Conner Donnelly <ConnerGDonnelly@gmail.com>
2022-07-13 16:04:17 -0500

    Commit to get us back to where we were before I lost all the data

--- Revature.P3.PickAndPayStore/Cart Page/CartViewModel.swift
+++ Revature.P3.PickAndPayStore/Cart Page/CartViewModel.swift
@@ -6,22 +6,21 @@
 //
 
 import Foundation
+import UIKit
 
 class cartViewModel{
     var subTotal:Double = 0.00
     var estimatedTax:Double = 0.00
     var total:Double = 0.00
+    let dialogLogin = UIAlertController(title: "Warning!", message: "You need to be logged in to place an order", preferredStyle: .alert)
     func subTotal (items: [Double]){
-//        var subTotal : Double = 0
         for item in items{
             subTotal = subTotal + item
         }
-        self.estimateTax(subTotal:subTotal)
     }
     func estimateTax(subTotal:Double){
         let tax = 0.06875
         estimatedTax = subTotal * tax
-        self.total(subTotal: subTotal, estimatedTax: estimatedTax)
     }
     
     func total (subTotal:Double,estimatedTax:Double){
@@ -32,5 +31,17 @@ class cartViewModel{
         subTotal = 0
         estimatedTax = 0
         total = 0
+    }
+    func recalc(items:[CartItemViewModel]){
+        var itemPrices:[Double]=[]
+        subTotal = 0
+        estimatedTax = 0
+        total = 0
+        for item in items{
+            itemPrices.append(Double(item.price) ?? 0)
+        }
+        self.subTotal(items: itemPrices)
+        self.estimateTax(subTotal: subTotal)
+        self.total(subTotal: subTotal, estimatedTax: estimatedTax)
     }
 }
