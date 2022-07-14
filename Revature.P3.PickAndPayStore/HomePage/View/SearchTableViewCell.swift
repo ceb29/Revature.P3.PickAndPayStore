465217f57fcabeda9eccd1d781a6ecd3aa14828c
Curtis Becker <ceb2941@gmail.com>
2022-07-14 19:37:59 -0400

    added search bar table

---
+++ Revature.P3.PickAndPayStore/HomePage/View/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  Revature.P3.PickAndPayStore
+//
+//  Created by admin on 7/14/22.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var searchLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
